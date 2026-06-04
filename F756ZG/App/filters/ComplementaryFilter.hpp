eba70e233dc0a53e82465dc18c2a4793e8d9fb7f
pavlo.baranchuk <pavlo.baranchuk@goreel.tech>
2026-06-04 20:03:30 +0300

    added filter calculation for x

--- F756ZG/App/filters/ComplementaryFilter.hpp
+++ F756ZG/App/filters/ComplementaryFilter.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <cstdint>
 
 class ComplementaryFilter {
+private:
+	static constexpr float alpha_ = 0.98f;
+	float currentAngle_{0.0f};
 
+public:
+	ComplementaryFilter() = default;
+
+	float update(float gyroX, float accX, float accY, float accZ, float dt = 0.01f);
 };
